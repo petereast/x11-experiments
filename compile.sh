bf018daba16d9afd2b7ea657c6d477fee8533987
Peter East <08peast@gmail.com>
2015-07-14 18:53:08 +0100

    Automated git update commital

---
+++ compile.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+## compile.sh -- This should compile the X11 stuff nicely
+
+##TODO: Do a g++ compile of everything *.cpp that can be found
+
+for directory in $(ls -R -1 -B -U -L | grep ":" | awk '{gsub(":", "/"); print}') 
+	do
+		for filename in $(ls $directory -B -1 | grep ".cpp")
+			do
+				target="$directory$filename"
+				g++ -Wall $target  -lX11  >> "${directory}compile.log"
+	done
+done
+
+
+##TODO: Do a git commit/push to the branch minutely_build
+
+git add .
+
+git commit --message="Automated git update commital"
+
+git push origin HEAD:minutely_build
