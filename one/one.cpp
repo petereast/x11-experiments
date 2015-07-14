1a015121b04b6aadef61a71404a2246f5d8c5fe5
Peter East <08peast@gmail.com>
2015-07-14 17:43:55 +0100

    Added a little, slightly working first attempt based on an online tutorial  Changes to be committed: 	new file:   one/one.cpp 	new file:   one/one.cpp~ 	new file:   one/out

---
+++ one/one.cpp
@@ -0,0 +1,62 @@
+#include <X11/Xlib.h>
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+
+
+int main(int argc, char* argv[])
+{
+	//printf("Hello world!");
+	
+	Display *dsp = XOpenDisplay( NULL );
+	if(! dsp){ return -1;}
+	
+	
+	int screen_number = DefaultScreen(dsp);
+	
+	unsigned long white WhitePixel(dsp, screen_number);
+	unsigned long black BlackPixel(dsp, screen_number);
+		
+	
+	Window win 	= XCreateSimpleWindow(dsp,
+									DefaultRootWindow(dsp),
+									50, 50,
+									400, 400,
+									0, black,
+									white);
+	XMapWindow(dsp, win);
+	
+	long event_mask = StructureNotifyMask;
+	XSelectInput( dsp, win, event_mask);
+	
+	XEvent evt;
+	
+	do{
+		XNextEvent(dsp, &evt); //Calls XFlush, whatever that is
+	}while( evt.type != MapNotify);
+	
+	GC gc = XCreateGC(dsp, win, 0, NULL );
+	
+	XSetForeground(dsp, gc, black);
+	
+	XDrawLine(dsp, win, gc, 10, 10, 100, 100);
+	XDrawLine(dsp, win, gc, 100, 10, 10, 100);
+	
+	event_mask = ButtonPressMask|ButtonReleaseMask;
+	XSelectInput(dsp, win, event_mask);
+	
+	do{
+		XNextEvent(dsp, &evt);
+		if(evt.type == ButtonRelease)
+		{
+			XDrawLine(dsp, win, gc, 0, 0, rand()*400, 400);
+		}
+	}while(true);
+	
+	XDestroyWindow(dsp, win);
+	XCloseDisplay(dsp);
+	
+	
+	
+	return 0;
+}
