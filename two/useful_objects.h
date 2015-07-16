3059d1223d1b480f72547f600f2af34e20ff6235
Peter East <08peast@gmail.com>
2015-07-16 20:35:20 +0100

    Automated git update commital

--- two/useful_objects.h
+++ two/useful_objects.h
@@ -24,15 +24,15 @@ TextBox::TextBox( Display *dsp, Window win, GC gc, char* text, int x, int y) {
     //Now draw a box around the text...
     printf("still working\n");
     int width = 100;//XTextWidth(font_info, text, string_length);
-    int height = 152;//font_info->ascent + font_info->descent;
+    int height = 12;//font_info->ascent + font_info->descent;
     printf("Not working anymore\n");
     int padding = 5; //Padding of 5 pixels
 
     //Draw the box;
     XDrawLine(dsp, win, gc, x-padding, y-padding, x+width+padding, y+padding); //Top
-    XDrawLine(dsp, win, gc, x-padding, y-padding, x-padding, y-padding-height); //left-side
-    XDrawLine(dsp, win, gc, x+width+padding, y-padding-height, x+width+padding, y+padding); //right side
-    XDrawLine(dsp, win, gc, x-padding, y-(height+padding), x+width+padding, y-height-padding); //bottom
+    //XDrawLine(dsp, win, gc, x-padding, y-padding, x-padding, y-padding-height); //left-side
+    //XDrawLine(dsp, win, gc, x+width+padding, y-padding-height, x+width+padding, y+padding); //right side
+    //XDrawLine(dsp, win, gc, x-padding, y-(height+padding), x+width+padding, y-height-padding); //bottom
   }
 
 
