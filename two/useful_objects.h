aba925abcf7db3222a548ef8bfa158c91091eb7d
Peter East <08peast@gmail.com>
2015-07-16 19:45:49 +0100

    Automated git update commital

--- two/useful_objects.h
+++ two/useful_objects.h
@@ -9,7 +9,7 @@
 class TextBox
 {
 public:
-  void TextBox(Display *dsp, Window win, GC gc, char* text, int x, int y);
+  TextBox(Display *dsp, Window win, GC gc, char* text, int x, int y);
 };
 
 TextBox::TextBox  {
