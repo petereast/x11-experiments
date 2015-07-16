0963dff42e73057d94243942b09ecdb08104221f
Peter East <08peast@gmail.com>
2015-07-16 21:00:14 +0100

    Automated git update commital

--- compile.sh
+++ compile.sh
@@ -10,7 +10,7 @@ for directory in $(ls -R -1 -B -U -L | grep ":" | awk '{gsub(":", "/"); print}')
 		for filename in $(ls $directory -B -1 | grep ".cpp")
 			do
 				target="$directory$filename"
-				g++ -Wall $target  -lX11 -o "$directory"out -Wwrite-strings >> "${directory}compile.log"
+				g++  $target  -lX11 -o "$directory"out -Wwrite-strings >> "${directory}compile.log"
 				echo "Built $target\nOutput to ${directory}out";
 	done
 done
