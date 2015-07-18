45c842ae08cd1d4f8b9e1e45525af67e23c851bb
Peter East <08peast@gmail.com>
2015-07-18 11:46:30 +0100

    Automated git update commital

--- compile.sh
+++ compile.sh
@@ -2,21 +2,16 @@
 
 ## compile.sh -- This should compile the X11 stuff nicely
 
-##TODO: Do a g++ compile of everything *.cpp that can be found
-##DONE
 
 for directory in $(ls -R -1 -B -U -L | grep ":" | awk '{gsub(":", "/"); print}')
 	do
 		for filename in $(ls $directory -B -1 | grep ".cpp")
 			do
 				target="$directory$filename"
-				$(g++  $target -lX11 -o "$directory"out )| grep "error" | echo
+				g++  $target -lX11 -o "$directory"out
 				echo "Built $target\nOutput to ${directory}out";
 	done
 done
-
-
-##TODO: Do a git commit/push to the branch minutely_build
 
 # Stage all the files to git
 git add . --all
