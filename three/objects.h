4548e4aa63a19db6bc5f881fdb4952a07bd8d394
Peter East <08peast@gmail.com>
2015-07-26 20:53:36 +0100

    A few updates, nothing really works quite yet, but we're getting there

---
+++ three/objects.h
@@ -0,0 +1,147 @@
+//Ensure the module isn't imported more than once
+#ifndef OBJECTS_H
+#define OBJECTS_H 0xF
+//Now that we're safe, include some modules
+#include <X11/Xlib.h>
+#include <stdio.h>
+#include <iostream>
+
+//finished including modules
+
+struct X11_data
+{
+  Display *display;
+  Window window;
+  XFontStruct fontInfo;
+  GC gc;
+  int screen_number;
+  unsigned long background_colour;
+  unsigned long foreground_colour;
+
+  unsigned long event_mask;
+
+  XEvent event;
+
+};
+
+X11_data initialise_x11(int window_height, int window_width)
+{
+  X11_data x;
+  x.display = XOpenDisplay( NULL );
+  if(!x.display){exit(-1);}
+
+  x.screen_number = DefaultScreen(x.display);
+
+  x.foreground_colour = WhitePixel(x.display, x.screen_number);
+  x.background_colour = BlackPixel(x.display, x.screen_number);
+
+  x.window = XCreateSimpleWindow(
+    x.display,
+    DefaultRootWindow(x.display),
+    0, 0,
+    window_height, window_width,
+    0, background_colour,
+    background_colour
+  );
+
+  XMapWindow(x.display, x.window);
+
+  x.event_mask = StructureNotifyMask;
+
+  do{
+    XNextEvent(x.display, &x.event);
+  }while(x.event.type != MapNotify);
+
+  x.gc = XCreateGC(x.display, x.window, 0, NULL);
+
+  XSetForeground(x.display, x.gc, x.foreground_colour);
+
+  return x;
+}
+
+class GraphicItem
+{
+protected:
+  char* id;
+  int x, y; //x and y positions of the item
+  int height, width; // height and width of the item
+  X11_data x_data;
+public:
+  //add some functions here!
+  char* getId();
+  void setId(char* id);
+};
+
+GraphicItem::getId()
+{
+  return id;
+}
+GraphicItem::setId(char* id)
+{
+  this->id = id;
+}
+
+class TextBox : public GraphicItem
+{
+protected:
+  char* text;
+  int padding = 5;
+public:
+  void Draw(bool initial); //Function to draw the textbox
+  void Update(int x, int y, char* text, char* font);
+  void TextBox();
+};
+
+void TextBox::Draw(bool initial)
+{
+  if(initial) //if this is the first time the function is run...
+  {
+    //calculate the dimentions of the text
+    int textwidth = XTextWidth(x_data.font_info, text, strlen(text));
+
+    this->width = textwidth+(padding*2);
+    this->height = x_data.font_info -> ascent + x_data.font_info -> descent + (padding*2);
+
+  }
+  //clear the area!
+  XClearArea(x_data.display, x_data.window, x-padding, y-padding, x+width, y+width, true);
+
+  //Draw the text
+  XDrawString(x_data.display, x_data.window, x_data.gc, x, y, text, strlen(text));
+
+  //Draw the box
+  XDrawLine(x_data.display, x_data.window, x_data.gc, x-padding, y-padding, x+width+padding, y-padding);
+  XDrawLine(x_data.diaplay, x_data.window, x_data.gc, x-padding, y-padding, x-padding, y+height+padding);
+  XDrawLine(x_data.display, x_data.window, x_data.gc, x-padding, y+height+padding, x+width+padding, y+height+padding);
+  XDrawLine(x_data.display, x_data.window, x_data.gc, x+width+padding, y-padding, x+width+padding, y+height+padding);
+}
+
+void TextBox::Update(int x, int y, char* text, bool initial)
+{
+  this -> x = x;
+  this -> y = y;
+  this -> text = text;
+
+  Draw(initial);
+
+}
+
+void TextBox::TextBox(X11_data& xd, int x, int y, char* text, char* font)
+{
+  this->x_data = xd;
+  //load a font...
+  if((x_data.font_info = XLoadQueryFont(x_data.display, font)) == NULL)
+  {
+    printf("[X11 ERROR] '%s' isn't a valid font, using 6x13 instead.");
+    x_data.font_info = XLoadQueryFont(x_data.display, "6x13");
+  }
+  XSetFont(x_data.display, x_data.window, x_data.gc, x_data.font_info->fid);
+
+  Update(x, y, text, true);
+}
+
+
+
+
+//end of header file
+#endif
