2e27aa1493099c8898509a7d4e05f5a791a1c606
Peter East <08peast@gmail.com>
2015-08-13 20:46:57 +0100

    Automated git update commital

--- three/three.cpp
+++ three/three.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[])
 {
   printf("hello world");
-  X11_data xd = initialise_x11(100, 100);
+  X11_data xd = initialise_x11(1000, 100);
   TextBox(xd, 12, 12, "HEllo world!", "9*15");
 
   while(true)
