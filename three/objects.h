27d23c93ddf52e1bcf0426caad8c586041a759c5
Peter East <08peast@gmail.com>
2015-08-13 18:51:13 +0100

    Automated git update commital

--- three/objects.h
+++ three/objects.h
@@ -136,7 +136,7 @@ void TextBox::TextBox(X11_data& xd, int x, int y, char* text, char* font)
   //load a font...
   if((x_data->fontInfo = XLoadQueryFont(x_data.display, font)) == NULL)
   {
-    printf("[X11 ERROR] '%s' isn't a valid font, using 6x13 instead.");
+    printf("[X11 ERROR] '%s' isn't a valid font, using 6x13 instead.", font);
     x_data.fontInfo = XLoadQueryFont(x_data.display, "6x13");
   }
   XSetFont(x_data.display, x_data.window, x_data.gc, x_data.fontInfo->fid);
