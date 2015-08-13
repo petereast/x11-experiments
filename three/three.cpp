ef5e49af6e4834ba04b051b3669328009a381483
Peter East <08peast@gmail.com>
2015-08-14 00:41:52 +0100

    Automated git update commital

--- three/three.cpp
+++ three/three.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
   addGraphicItem(tb);
   //TODO: Create a reference system that will allow events to be passed to and from
   //using a really easy function like OGetNextEvent(xd, [XEventMask])
-  while(getNextEvent(xd, StructureNotifyMask | KeyPressMask))
+  while((getNextEvent(xd, StructureNotifyMask | KeyPressMask) != NULL)
   {
     //printf("test\n");
     tb.Draw(false);
