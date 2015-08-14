bab99df815acf6ed94cfad585cc3d46d286684ae
Peter East <08peast@gmail.com>
2015-08-14 17:20:07 +0100

    Added stuff to the thing

--- four/objects.h
+++ four/objects.h
@@ -1,10 +1,26 @@
-//safety
 #ifndef OBJECTS_H
 #define OBJECTS_H 0x1
-//safety all done
+//README:
+//This is the header containing all of the functions and classes used for creating
+//and adjusting the graphics objects.
 
-//define the graphic item classes
+//DEFINE the macros:
+#define LINE 0x00
+#define RECT 0x01
+#define TEXTBOX 0x02
+#define BUTTON 0x03
+#define ENTRY 0x04
+#define CHECKBOX 0x05
+#define LABEL 0x06
+#define WRAPPED_TEXT 0x07
 
+struct Attributes{ //This will contain all of the possible attributes for the graphics
+                  //items, it'll be a member of the parent class so that it'll always
+                 //be accessable regardless of the type.
+  int TYPE; //This'll be a macro from 0 to whatever, to be added as we go along
+
+
+};
 
 
 
--- four/render.h
+++ four/render.h
@@ -5,5 +5,44 @@
 //defined in "objects.h". It will also contain all of the classes for adding new
 //elements to the rendering stack
 
+X11_data initialise_x11(int window_height, int window_width)
+{
+  fprintf(stdout, "[ln32]");
+  X11_data x;
+  x.display = XOpenDisplay( NULL );
+  if(!x.display){exit(-1);}
+
+  x.screen_number = DefaultScreen(x.display);
+
+  x.foreground_colour = WhitePixel(x.display, x.screen_number);
+  x.background_colour = BlackPixel(x.display, x.screen_number);
+  x.window = XCreateSimpleWindow(
+    x.display,
+    DefaultRootWindow(x.display),
+    0, 0,
+    window_height, window_width,
+    0, x.background_colour,
+    x.background_colour
+  );
+
+  XMapWindow(x.display, x.window);
+
+  x.event_mask = StructureNotifyMask;
+  fprintf(stdout, "[ln53]");
+  XSelectInput(x.display, x.window, x.event_mask);
+  do{
+    XNextEvent(x.display, &x.event);
+  }while(x.event.type != MapNotify);
+  printf("[ln58]");
+
+
+  x.gc = XCreateGC(x.display, x.window, 0, NULL);
+
+  XSetForeground(x.display, x.gc, x.foreground_colour);
+
+  return x;
+}
+
+
 
 #endif
