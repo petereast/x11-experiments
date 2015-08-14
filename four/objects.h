f425445117b053f2723cbf7caece5dd5232c3e09
Peter East <08peast@gmail.com>
2015-08-14 16:44:50 +0100

    slight update

--- four/objects.h
+++ four/objects.h
@@ -1,2 +1,11 @@
+//safety
 #ifndef OBJECTS_H
 #define OBJECTS_H 0x1
+//safety all done
+
+//define the graphic item classes
+
+
+
+
+#endif
---
+++ four/render.h
@@ -0,0 +1,9 @@
+#ifndef RENDER_H
+#define RENDER_H 0x1
+//README:
+//This header will contain all of the functions which use the descriptive classes
+//defined in "objects.h". It will also contain all of the classes for adding new
+//elements to the rendering stack
+
+
+#endif
